07ddfb0d53c050ffe703a88322666a423d9f4d3c
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2023-02-08 02:34:43 -0300

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,12 @@
+import cheerio from 'cheerio';
+
+
+const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+$('h2.title').text('Hello there!')
+$('h2').addClass('welcome')
+    
+console.log($.html());
+
+
+    
+
