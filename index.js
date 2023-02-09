9321749aa629e64027574b076758f17204369d2a
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2023-02-09 17:21:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
-import cheerio from 'cheerio';
+import { next } from 'cheerio/lib/api/traversing';
+import express from 'express';
+import handlebars from 'express-handlebars';
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local'
+LocalStrategy.Strategy;
+const app = express();
+const PORT = process.env.PORT || 3000;
+const engine = handlebars.create({
+    defaultLayout: 'main',
+    layoutsDir: 'views/layouts',
+    partialsDir: 'views/partials',
+    extname: 'hbs'
+});
 
 
-const $ = cheerio.load('<h2 class="title">Hello world</h2>')
-$('h2.title').text('Hello there!')
-$('h2').addClass('welcome')
-    
-console.log($.html());
+app.engine('hbs', engine.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 6000 }
+}));
+passport.use(new LocalStrategy.Strategy(
+    function (username, password, done) {
+        if (username === 'admin' && password === 'admin') {
+            return done(null, { username: 'admin', password: 'admin' });
+        } else {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+    }
+));
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-    
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.post('/login', (req, res) => {
+
+
+
+
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+    }
+);
